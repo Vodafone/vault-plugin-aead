c1f46a5c3384353ed21532cca3c43c9295f7a0d6
Piotr Ged <piotr.ged1@vodafone.com>
2022-07-15 02:30:16 +0200

    Add SA login to performance test

--- cmd/performance/performance.sh
+++ cmd/performance/performance.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 ./performance -f 6 -r 1000 -c 100 -i 100 -t <a-vault-token> -u <vault-url> -b -s -col
+
+
+# /performance -f 6 -r 1 -c 1 -i 1 --sa "/path/to/sa-key.json"  --u "https://beta-eaas.rubik.vodafone.com" --vr "encryptor-decryptor-common-nonlive-iam"  --path common-nonlive/aead -b -col -p http://vfukukproxy.internal.vodafone.com:8080
