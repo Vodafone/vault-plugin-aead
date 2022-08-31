91a7531fa4b4e36a87602f515b3da59b59dc9190
pg-sp <81873938+pg-sp@users.noreply.github.com>
2022-08-31 09:46:05 +0200

    remove specificity

--- cmd/performance/performance.sh
+++ cmd/performance/performance.sh
@@ -3,4 +3,4 @@
 ./performance -f 6 -r 1000 -c 100 -i 100 -t <a-vault-token> -u <vault-url> -b -s -col
 
 
-# /performance -f 6 -r 1 -c 1 -i 1 --sa "/path/to/sa-key.json"  --u "https://beta-eaas.rubik.vodafone.com" --vr "encryptor-decryptor-common-nonlive-iam"  --path common-nonlive/aead -b -col -p http://vfukukproxy.internal.vodafone.com:8080
+# /performance -f 6 -r 1 -c 1 -i 1 --sa "/path/to/sa-key.json"  --u "https://vault.example.com" --vr "encryptor-decryptor-example-iam"  --path example-aead -b -col -p http://proxy.example.com:8000
